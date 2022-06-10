69ed69f7eae3c7450edc0548dc9ba16ae2ca6bcb
thuc nhi <thuc-nhi.wiedenhofer@laolateforme.io>
2022-06-10 22:49:58 +0200

    page 404

--- src/pages/Page404.jsx
+++ src/pages/Page404.jsx
@@ -1,11 +1,18 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 import Header from "../components/Header/Header";
 
 const Page404 = () => {
     return (
     <div>
         <Header/>
-        <h1>Page 404</h1>
+        <div className="container-fluid">            
+                <h1 className="error">404</h1>
+                <h2 className="text-center m-4">Oups! La page que vous demandez n'existe pas.</h2>
+                <NavLink  to="/">
+                    <p className="backHome">Retourner sur la page d'accueil</p>
+                </NavLink>
+        </div>        
     </div>
     );
 };
